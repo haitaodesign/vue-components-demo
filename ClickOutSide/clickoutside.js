50ba76af2dbf3e3abecd1c111ba60aa3a692b193
haitaodesign <1775814590@qq.com>
2017-11-02 23:02:29 +0800

    自定义指令内添加ESC键关闭下拉菜单

--- ClickOutSide/clickoutside.js
+++ ClickOutSide/clickoutside.js
@@ -8,11 +8,20 @@ Vue.directive('clickoutside', {
                 binding.value(e);
             }
         }
+        function documentESC(e){
+            if (binding.expression) {
+                binding.value(e);
+            }
+        }
         el._vueClickOutside_ = documentHandler;
+        el._vueESCOutside_ = documentESC;
         document.addEventListener('click', documentHandler);
+        document.addEventListener('keydown', documentESC);
     },
     unbind: function (el, binding) {
         document.removeEventListener('click', el._vueClickOutside_);
+        document.removeEventListener('keydown', el._vueESCOutside_);
+        delete el._vueESCOutside_;
         delete el._vueClickOutside_;
     }
 })
