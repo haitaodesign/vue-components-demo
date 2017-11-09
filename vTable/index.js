80dc321ab150e637176bbf869addc8230dd3a8a6
haitaodesign <1775814590@qq.com>
2017-11-09 23:24:21 +0800

    可排序表格组件完成

--- vTable/index.js
+++ vTable/index.js
@@ -9,14 +9,25 @@ var app = new Vue({
                 title: '年龄',
                 key: 'age',
                 sortable: true
+            }, {
+                title: '出生日期',
+                key: 'birthday'
+            },{
+                title: '地址',
+                key: 'address'
             }
         ],
-        data:[
+        data: [{
+                name: '王小明',
+                age: 30,
+                birthday: '1999-02-21',
+                address: '北京市朝阳区芍药居'
+            },
             {
-                name:'王小明',
-                age:18,
-                brithday:'1999-02-21',
-                address:'北京市朝阳区芍药居'
+                name: '张小刚',
+                age: 25,
+                birthday: '1992-03-21',
+                address: '北京市海淀区西二旗'
             }
         ]
     }
